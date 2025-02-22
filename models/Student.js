09134e9b95e12fe621b9101e531ecdf4114a243d
TKhoi <153786779+TuanKhoi67@users.noreply.github.com>
2025-02-22 22:56:32 +0700

    change2

--- models/Student.js
+++ models/Student.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const StudentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  advisor: { type: mongoose.Schema.Types.ObjectId, ref: 'Advisor' }
+  advisor: { type: mongoose.Schema.Types.ObjectId, ref: 'Tutor' }
 });
 module.exports = mongoose.model('Student', StudentSchema);
--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,8 @@
-// models/User.js
+// models/Users.js
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'tutor', 'student'], required: true }
 });
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Users', UserSchema);
